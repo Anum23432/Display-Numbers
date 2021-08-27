f90404aed3f1cae51f354fd24afd047ffd3a4e34
Anum23432 <anumaneesshah@gmail.com>
2021-08-27 13:41:10 +0500

    latest deployment

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import ListItem from './components/listitems';
+
+import './App.css';
+
+function App() {
+
+ 
+  
+  return (
+    <div className="App">
+      
+        <ListItem/>
+    
+    </div>
+  );
+}
+
+export default App;
